6ba1ad00fce0e26454283f20dba136cbd5c00bd2
Pete Bachant <petebachant@gmail.com>
2015-02-11 17:14:27 -0500

    Modify plotting script

--- plot.py
+++ plot.py
@@ -2,14 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 plt.style.use("ggplot")
 
-def plot(name):
-    df = pd.read_csv("closed-jet_{}.csv".format(name))
+def plot(name, env="reference"):
+    df = pd.read_csv("{}_{}.csv".format(env, name))
     plt.plot(df.alpha, df.cl/df.cd, "-o", label="$Re = {}$".format(name))
     
 def main():
+    env = "open-jet"
     names = ["6e4", "1e5", "1.4e5"]
     for name in names:
-        plot(name)
+        plot(name, env)
     plt.xlabel(r"$\alpha$")
     plt.ylabel("$C_l/C_d$")
     plt.legend()
